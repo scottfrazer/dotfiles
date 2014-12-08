1bf321514672542c212d503fbf287deab34b463d
Scott Frazer <scott.d.frazer@gmail.com>
2014-12-08 15:30:51 +0000

    https clones

--- setup.sh
+++ setup.sh
@@ -11,15 +11,14 @@ popd
 pushd ~/.vim
 git clone https://github.com/gregsexton/MatchTag bundle/MatchTag
 git clone https://github.com/kien/ctrlp.vim.git bundle/ctrlp
-git clone git://github.com/docunext/closetag.vim.git bundle/closetag
-git clone git@github.com:derekwyatt/vim-scala.git bundle/vim-scala
-git clone git@github.com:elzr/vim-json.git bundle/vim-json
-git clone git://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
-git clone git@github.com:vim-scripts/taglist.vim.git bundle/taglist
-git clone https://github.com/scrooloose/syntastic.git bundle/syntastic
-git clone git://github.com/tpope/vim-fugitive.git bundle/vim-fugitive
+git clone https://github.com/docunext/closetag.vim.git bundle/closetag
+git clone https://github.com/derekwyatt/vim-scala.git bundle/vim-scala
+git clone https://github.com/elzr/vim-json.git bundle/vim-json
+git clone https://github.com/altercation/vim-colors-solarized.git bundle/vim-colors-solarized
+git clone https://github.com/vim-scripts/taglist.vim.git bundle/taglist
+git clone https://github.com/tpope/vim-fugitive.git bundle/vim-fugitive
 git clone https://github.com/bronson/vim-trailing-whitespace bundle/vim-trailing-whitespace
-git clone git://github.com/majutsushi/tagbar.git bundle/tagbar
+git clone https://github.com/majutsushi/tagbar.git bundle/tagbar
 git clone https://github.com/Lokaltog/vim-powerline bundle/vim-powerline
 git clone https://github.com/scrooloose/nerdtree.git bundle/nerdtree
 git clone https://github.com/flazz/vim-colorschemes bundle/vim-colorschemes
